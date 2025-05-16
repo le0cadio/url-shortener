2865c1c5bcf5f5accedc99a3955876e98587a68f
le0cadio <guilhermeleocadio1301@gmail.com>
2025-05-16 18:41:26 -0300

    implement URL shortening and redirection functionality

--- backend/src/main/kotlin/com/example/Application.kt
+++ backend/src/main/kotlin/com/example/Application.kt
@@ -1,10 +1,14 @@
 package com.example
 
+import com.example.controllers.UrlController
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import com.example.dao.ShortUrlDao
+import io.ktor.http.*
+import io.ktor.server.request.*
 
 
 fun main() {
@@ -15,13 +19,40 @@ fun main() {
 
 fun Application.module() {
     DatabaseFactory.init()
-
+    val dao = ShortUrlDao()
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        val controller = UrlController()
+
+        post("/shorten") {
+            try {
+                val params = call.receiveParameters()
+                val originalUrl = params["url"] ?: return@post call.respondText("Missing URL", status = HttpStatusCode.BadRequest)
+                val shortUrl = controller.shortenUrl(originalUrl)
+                call.respondText("Shortened URL: http://localhost:8080/${shortUrl.shortCode}")
+            } catch (e: Exception) {
+                e.printStackTrace()
+                call.respondText("Internal Server Error: ${e.message}", status = HttpStatusCode.InternalServerError)
+            }
         }
-        post("/") {
-            call.respondText("Hello World! 1")
+
+        /* post("/shorten") {
+            val params = call.receiveParameters()
+            val originalUrl =
+                params["url"] ?: return@post call.respondText("Missing URL", status = HttpStatusCode.BadRequest)
+            val shortUrl = controller.shortenUrl(originalUrl)
+            call.respondText("Shortened URL: http://localhost:8080/${shortUrl.shortCode}")
+        } */
+
+        get("/{code}") {
+            val code = call.parameters["code"]
+            val shortUrl = code?.let { controller.getOriginalUrl(it) }
+
+            if (shortUrl == null) {
+                call.respondText("Not Found", status = HttpStatusCode.NotFound)
+            } else {
+                call.respondRedirect(shortUrl.originalUrl)
+            }
         }
+
     }
 }
